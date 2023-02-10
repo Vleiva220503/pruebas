59c14854cc062c1e7a201b206f7976832fd96922
Vleiva220503 <leivavictor220503@gmail.com>
2023-02-10 16:10:49 -0600

    practica

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,68 @@
+import { useState } from "react";
 import Boton from "./components/Boton";
 
 const App = () => {
-  const c = "correo";
-  const p = "password";
+  const [form, setForm] = useState({
+    correo: "",
+    password: "",
+  });
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+  const handleSubmit = (e) => {
+    const { name, value } = e.target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
 
   return (
     <div className="contenedor">
-      <h2 className="l">Empresa</h2>
-      <p className="l">--Ingrese sus credenciales--</p>
-      <form id="forma" name="forma" method="POST">
-        <div className="p">
+      <h2 className="h2">Iniciar Sesion</h2>
+      <form onSubmit={submit} id="forma" name="forma" method="POST">
+        <div className="email">
           <input
-            className="c"
-            placeholder="Correo"
+            className="in"
+            placeholder="Email o number"
             type="text"
             id="correo"
             name="correo"
+            value={form.correo}
             required="true"
+            onChange={handleSubmit}
           />
         </div>
 
-        <div className="p">
+        <div className="email">
           <input
-            className="c"
-            placeholder="Contraseña"
+            className="in"
+            placeholder="Password"
             type="password"
             id="password"
             name="password"
+            value={form.password}
+            onChange={handleSubmit}
             required="true"
           />
         </div>
+        <div className="l">
+          <div>
+            <input type="checkbox" />
+            <label>Recuerdame</label>
+          </div>
 
-        <button className="b1">Iniciar Sesion</button>
+          <label className="p">Necesitas Ayuda?</label>
+        </div>
+
+        <button type="submit" className="b1">Iniciar Sesion</button>
+        <div className="c">
+          Esta paginna esta protegida por google recapacha para comprobar que no
+          eres un robot
+        </div>
       </form>
     </div>
   );
